4f30290f2112885c276ceb5427df30d95896291f
KittenLord <vanya.braslavets@gmail.com>
2024-11-02 19:01:20 +0200

    supposedly the endianness is correct now

--- ipv4.c
+++ ipv4.c
@@ -39,18 +39,22 @@ struct __attribute__((packed)) IPv4OptionType {
 
 #define MIN_IPV4_HEADER_LENGTH 5
 struct __attribute__((packed)) IPv4Header {
-    unsigned int                version : 4;
+    // version has to take the most significant 4 bits of the octet, hence the order
     unsigned int                length : 4;
+    unsigned int                version : 4;
+
     struct IPv4TypeOfService    typeOfService;
     int16_t                     totalLength;
 
     int16_t                     identification;
 
+    unsigned int                fragmentOffsetHi : 5;
+
     unsigned int                _reserved : 1;
     unsigned int                dontFragment : 1;
     unsigned int                moreFragments : 1;
 
-    unsigned int                fragmentOffset : 13;
+    unsigned int                fragmentOffsetLo : 8;
 
     int8_t                      timeToLive;
     int8_t                      protocol;
@@ -59,6 +63,25 @@ struct __attribute__((packed)) IPv4Header {
     struct IPv4Address          sourceAddress;
     struct IPv4Address          destinationAddress;
 };
+
+bool isLittleEndian() {
+    int16_t n = 1;
+    return *(int8_t *)&n;
+}
+
+int16_t hn16(int16_t n) {
+    if(!isLittleEndian()) return n;
+    return ((n >> 8) & 0x00FF) | ((n << 8) & 0xFF00);
+}
+
+void setFragmentOffset(struct IPv4Header *header, uint16_t offset) {
+    header->fragmentOffsetLo = offset & 0xFF;
+    header->fragmentOffsetHi = (offset >> 8) & 0b11111;
+}
+
+uint16_t getFragmentOffset(struct IPv4Header *header) {
+    return (header->fragmentOffsetHi << 8) | header->fragmentOffsetLo;
+}
 
 void assignChecksum(struct IPv4Header *header) {
     header->checksum = 0;
@@ -71,7 +94,7 @@ void assignChecksum(struct IPv4Header *header) {
         sum = (psum & 0xFFFF) + (psum >> 16);
         sbp++;
     }
-    header->checksum = sum;
+    header->checksum = hn16(sum);
 }
 
 bool hasOptions(struct IPv4Header *header) {
@@ -79,7 +102,7 @@ bool hasOptions(struct IPv4Header *header) {
 }
 
 bool hasData(struct IPv4Header *header) {
-    return (header->length * 4) < (header->totalLength);
+    return (header->length * 4) < (hn16(header->totalLength));
 }
 
 int findHeaderOptionsOffset(struct IPv4Header *header) {
@@ -106,18 +129,18 @@ int findHeaderOptionsOffset(struct IPv4Header *header) {
 
 struct IPv4Header *addOption(struct IPv4Header *header, struct IPv4OptionType optionType, int8_t optionLength, int8_t *optionData) {
     int oldLength = header->length;
-    int oldTotalLength = header->totalLength;
+    int oldTotalLength = hn16(header->totalLength);
 
     int insertOffset = findHeaderOptionsOffset(header);
     int newLength = insertOffset + (optionLength == 0 ? 1 : optionLength);
     while(newLength % 4 != 0) newLength++;
 
     newLength /= 4;
-    int newTotalLength = header->totalLength + (newLength - header->length)*4;
+    int newTotalLength = oldTotalLength + (newLength - oldLength)*4;
 
-    if(newLength >= header->length) header = realloc(header, newTotalLength);
+    if(newLength >= oldLength) header = realloc(header, newTotalLength);
     header->length = newLength;
-    header->totalLength = newTotalLength;
+    header->totalLength = hn16(newTotalLength);
 
     if(hasData(header)) memmove((int8_t *)header + newLength*4, (int8_t *)header + oldLength*4, oldTotalLength - oldLength*4);
 
@@ -139,7 +162,7 @@ struct IPv4Header *addOption(struct IPv4Header *header, struct IPv4OptionType op
 struct IPv4Header *setData(struct IPv4Header *header, int16_t size, int8_t *data) {
     int16_t newTotalLength = header->length*4 + size;
     header = realloc(header, newTotalLength);
-    header->totalLength = newTotalLength;
+    header->totalLength = hn16(newTotalLength);
     int8_t *dest = (int8_t *)header + header->length*4;
     memcpy(dest, data, size);
     return header;
@@ -149,7 +172,7 @@ struct IPv4Header *createPacket() {
     struct IPv4Header *header = calloc(1, sizeof(struct IPv4Header));
     header->version = 4;
     header->length = MIN_IPV4_HEADER_LENGTH;
-    header->totalLength = MIN_IPV4_HEADER_LENGTH*4;
+    header->totalLength = hn16(MIN_IPV4_HEADER_LENGTH*4);
     return header;
 }
 
--- main.c
+++ main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <linux/if_ether.h>
 
 #include "ipv4.c"
 
+#include "../pretty-dump/prettydump.c"
+
 int main() {
 
-    char *test = "Hello, World!";
+    char *msg = "Hello, World!";
 
     struct IPv4Header *header = createPacket();
     struct IPv4OptionType opt = { .isCopied = 0, .class = 0, .number = 1 };
 
-    header = addOption(header, opt, 0, 0);
-    header = setData(header, strlen(test), test);
+    printf("%d\n", sizeof(struct IPv4Header));
 
-    int sfd = socket(AF_PACKET, SOCK_RAW, ETH_P_ALL);
-    printf("%d\n", sfd);
+    header->_reserved = 1;
+    header->dontFragment = 1;
+    header->moreFragments = 1;
+
+    setFragmentOffset(header, 7000);
+
+    uint16_t offset = getFragmentOffset(header);
+    printf("Offset %d\n", offset);
+
+    header->timeToLive = 0xFF;
+    header->protocol = 6;
+
+    header = addOption(header, opt, 0, 0);
+    header = setData(header, strlen(msg), msg);
+
+    prdump(header, hn16(header->totalLength), PRD_HEX, PRD_ONLY_OFFSET, 1, 4, 2);
+    prdump(header, hn16(header->totalLength), PRD_ASCII, PRD_ONLY_OFFSET, 1, 4, 2);
 
     return 0;
 }
